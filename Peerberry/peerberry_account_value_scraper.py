7d20df49b8b1cb140a27a684e51103dc827893db
Søren Krogh <soren1@hotmail.com>
2019-08-18 14:55:19 +0200

    Adds support for Peerberry

--- Peerberry/peerberry_account_value_scraper.py
+++ Peerberry/peerberry_account_value_scraper.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-host = "https://www.mintos.com/en"
+host = "https://peerberry.com/en"
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 browser = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver', chrome_options = options)
@@ -15,25 +15,30 @@ wait = WebDriverWait(browser, timeout = 10) # seconds
 def login():
     # Navigate to login page
     browser.get(host + "/")
-    account = getElement(By.ID, 'header-login-button')
+    header = browser.find_element_by_class_name('Header')
+    boarder_bottom = browser.find_element_by_class_name('border-bottom')
+    account = browser.find_element_by_class_name('login')
     account.click()
     time.sleep(1) # workaround: javascript needs to be loaded
     
     # Fill login form and submit
     credentials = get_credentials()
-    username = getElement(By.NAME, '_username')
+    username = browser.find_element_by_name('email')
     username.send_keys(credentials['usr'])
-    password = browser.find_element_by_name('_password')
+    password = browser.find_element_by_name('password')
     password.send_keys(credentials['pwd'])
-    form = browser.find_element_by_id('login-form')
+    login = browser.find_element_by_class_name('PAGE')
+    form = login.find_element_by_tag_name('form')
     form.submit()
     
     # Retreive total value of account
-    overview_box = getElement(By.CLASS_NAME, 'overview-box')
-    value_element = overview_box.find_element_by_class_name('value')
-    account_value = value_element.text.split('€')[1].strip()
+    account_info = getElement(By.CLASS_NAME, 'account-info')
+    account_balance = account_info.find_elements_by_class_name('row')
+    total_div = account_balance[1].find_elements_by_class_name('col-sm-4')
+    total_value_component = total_div[2].find_element_by_tag_name('div')
+    total_value = total_value_component.text.split('€')[0].strip()
     
-    return account_value
+    return total_value
 
 
 def getElement(by, name):
@@ -49,7 +54,7 @@ def quit():
 def get_credentials():
     # read json file data from previous run
     with open("../credentials.json") as f_check:
-        data = json.load(f_check)['mintos']
+        data = json.load(f_check)['peerberry']
     return data
 
 
