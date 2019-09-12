07869a6b19bb51e4521bc22e327cfe68960fb662
Søren Krogh <soren1@hotmail.com>
2019-09-12 18:40:49 +0200

    updates gitignore and. Minor changes to browser.py

--- utils/browser.py
+++ utils/browser.py
@@ -10,6 +10,8 @@ class Browser:
 	def __init__(self):
 		self.options = webdriver.ChromeOptions()
 		self.options.add_argument('headless')
+		self.options.add_argument('--no-sandbox')
+		self.options.add_argument('--disable-dev-shm-usage')
 		self.browser = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver', chrome_options = self.options)
 		self.wait = WebDriverWait(self.browser, timeout = 10) # seconds
 
